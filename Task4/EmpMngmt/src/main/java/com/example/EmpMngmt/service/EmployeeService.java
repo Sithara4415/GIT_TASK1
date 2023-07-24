91e67a69fd1859d8d6c61728bb1a38a3f5cb6f74
Sithara B Raj <119795602+Sithara4415@users.noreply.github.com>
2023-07-24 18:17:13 +0530

    Add files via upload

---
+++ Task4/EmpMngmt/src/main/java/com/example/EmpMngmt/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.EmpMngmt.service;
+
+import com.example.EmpMngmt.dto.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface EmployeeService {
+
+    public Employee addEmployee(Employee employee);
+
+    public Optional<Employee> getEmployee(int empId);
+
+    public String deleteEmployee(Employee deletedUser);
+
+    public String updateEMPLOYEE(eMPLOYEE updateemployee);
+}
